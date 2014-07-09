e8c8da6d188d87f1361f24376ad5285f27c7a1b6
yuzuoninggithub <893266239@qq.com>
2014-07-09 17:22:58 +0800

    按钮小修改

--- gen/com/imcore/xbionic/R.java
+++ gen/com/imcore/xbionic/R.java
@@ -1161,10 +1161,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05005b;
+        public static final int action_settings=0x7f050061;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btn_creat_user=0x7f05005f;
         public static final int btn_qq_accredit=0x7f050047;
         public static final int btn_qq_cancel=0x7f050048;
         public static final int btn_sina_cancel=0x7f05004b;
@@ -1176,6 +1177,9 @@ containing a value of this type.
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int ed_password_login=0x7f05005d;
+        public static final int ed_telephone_number=0x7f05005c;
+        public static final int ed_user_email=0x7f05005e;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int et_password_login=0x7f05003f;
@@ -1184,11 +1188,12 @@ containing a value of this type.
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
+        public static final int ibtn_back_register=0x7f05005b;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int img_spot=0x7f05004f;
-        public static final int iv_bg=0x7f05005a;
+        public static final int iv_bg=0x7f050060;
         public static final int iv_btn_back=0x7f05003d;
         public static final int iv_bulo_login=0x7f050040;
         public static final int iv_help1=0x7f050058;
@@ -1214,6 +1219,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int rl_top=0x7f05005a;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1300,7 +1306,8 @@ containing a value of this type.
         public static final int support_simple_spinner_dropdown_item=0x7f03001c;
         public static final int view_image_item=0x7f03001d;
         public static final int view_level_login=0x7f03001e;
-        public static final int view_welcome=0x7f03001f;
+        public static final int view_telephone_register=0x7f03001f;
+        public static final int view_welcome=0x7f030020;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/imcore/xbionic/ui/NewuserRegister.java
+++ src/com/imcore/xbionic/ui/NewuserRegister.java
@@ -1,16 +1,30 @@
 package com.imcore.xbionic.ui;
 
-import com.imcore.xbionic.R;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class NewuserRegister extends ActionBarActivity {
+import com.imcore.xbionic.R;
+
+public class NewuserRegister extends ActionBarActivity implements OnClickListener {
+	private Button btnTelephoneRegister;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_level_register);
 		getSupportActionBar().hide();
+		btnTelephoneRegister = (Button) findViewById(R.id.iv_telephone_register);
+		
+		btnTelephoneRegister.setOnClickListener(this);
+		
+	}
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, TelephoneRegister.class);
+		startActivity(intent);
 	}
 }
