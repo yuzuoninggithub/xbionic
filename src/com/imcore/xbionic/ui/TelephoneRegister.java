05039350063c87a2c0bbe2db49c5da45b6383fb2
yuzuoninggithub <893266239@qq.com>
2014-07-10 17:36:54 +0800

    各界面小修改

--- src/com/imcore/xbionic/ui/TelephoneRegister.java
+++ src/com/imcore/xbionic/ui/TelephoneRegister.java
@@ -2,15 +2,31 @@ package com.imcore.xbionic.ui;
 
 import com.imcore.xbionic.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class TelephoneRegister extends ActionBarActivity{
+public class TelephoneRegister extends ActionBarActivity implements OnClickListener{
+	private Button btnTeleRegister;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.view_telephone_register);
+		setContentView(R.layout.view_tele_register);
+		getSupportActionBar().hide();
+		
+		btnTeleRegister = (Button) findViewById(R.id.btn_tele_register);
+		btnTeleRegister.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, TeleRegister.class);
+		startActivity(intent);
 	}
 }
